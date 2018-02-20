3bc05631d7e12f8959f8c1b059756d03e6dd1f7d
Henry Lam <nkb012pomm@gmail.com>
2018-02-20 16:07:44 -0700

    Set up a basic android studio app activity early to set up test cases quicker when we get to it.

---
+++ app/src/main/java/com/c301t3/c301t3app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.c301t3.c301t3app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
