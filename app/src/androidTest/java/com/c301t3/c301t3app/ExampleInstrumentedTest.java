1dbf6cb9d3c0eae56f2e9aaba0823479598bb811
xwafflecakex <nyevtukh@gmail.com>
2018-02-25 21:38:45 -0800

    Added comment for clarification
    can change as project is done

--- app/src/androidTest/java/com/c301t3/c301t3app/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/c301t3/c301t3app/ExampleInstrumentedTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
- *
+ * Subject to change.
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
