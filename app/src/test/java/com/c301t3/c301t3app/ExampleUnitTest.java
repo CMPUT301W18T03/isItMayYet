25f02834737fdb2c75d02ac47321823f4ad4aa51
xwafflecakex <nyevtukh@gmail.com>
2018-02-25 21:39:16 -0800

    Update ExampleUnitTest.java

--- app/src/test/java/com/c301t3/c301t3app/ExampleUnitTest.java
+++ app/src/test/java/com/c301t3/c301t3app/ExampleUnitTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
- *
+ * Subject to Change
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
