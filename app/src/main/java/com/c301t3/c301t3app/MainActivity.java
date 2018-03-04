e678eaba143c2d67a8a59a954ff27fe901a05a3f
xwafflecakex <nyevtukh@gmail.com>
2018-03-03 20:12:32 -0800

    Created view 5 of story board. Also did an example of how to make back button with activities.

--- app/src/main/java/com/c301t3/c301t3app/MainActivity.java
+++ app/src/main/java/com/c301t3/c301t3app/MainActivity.java
@@ -1,13 +1,27 @@
 package com.c301t3.c301t3app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    /* Here is a good site with a good tutorial for back button and info sharing between activities.
+    * https://google-developer-training.gitbooks.io/android-developer-fundamentals-course-practicals/content/en/Unit%201/21_p_create_and_start_activities.html*/
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, selected_task_activity.class);
+        startActivity(intent);
     }
 }
