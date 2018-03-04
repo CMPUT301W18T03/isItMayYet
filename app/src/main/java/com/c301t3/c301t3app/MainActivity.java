6f67aa5a0491afccd6ebdf43d8d7018c1d65ad40
Henry Lam <nkb012pomm@gmail.com>
2018-03-03 23:25:20 -0700

    This is the implementation of View3, data extraction and projection from intents not yet completed, which will be my next focus.

--- app/src/main/java/com/c301t3/c301t3app/MainActivity.java
+++ app/src/main/java/com/c301t3/c301t3app/MainActivity.java
@@ -1,13 +1,41 @@
 package com.c301t3.c301t3app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int MYTASKCODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button GoToMyTasks = (Button) findViewById(R.id.button_GoToMyTasks);
+
+        GoToMyTasks.setOnClickListener( new View.OnClickListener() {
+
+            public void onClick(View view) {
+                setResult(RESULT_OK);
+                Intent intent = new Intent(view.getContext(), myTasksActivity.class);
+
+                Task task0 = new Task("task0", "description for task0", "okay");
+                Task task1 = new Task("task1", "description for task1", "meh");
+                Task task2 = new Task("task2", "description for task2", "cool");
+                TaskList tasklist = new TaskList();
+                tasklist.addTask(task0);
+                tasklist.addTask(task1);
+                tasklist.addTask(task2);
+
+                intent.putExtra("SentTaskList", tasklist);
+                startActivityForResult(intent, MYTASKCODE);
+            }
+
+        });
+
     }
 }
