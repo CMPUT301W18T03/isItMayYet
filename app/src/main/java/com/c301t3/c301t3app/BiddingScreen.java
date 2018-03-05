0754425839db44a30e0b8440920e4a4daa1790f2
Ravenshard <jechmila@ualberta.ca>
2018-03-04 20:54:16 -0700

    Thinking view4 is finished

--- app/src/main/java/com/c301t3/c301t3app/BiddingScreen.java
+++ app/src/main/java/com/c301t3/c301t3app/BiddingScreen.java
@@ -3,11 +3,11 @@ package com.c301t3.c301t3app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class BiddingScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.biddingscreen);
     }
 }
