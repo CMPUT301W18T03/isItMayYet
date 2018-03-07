75d52ee00581b6029dc9ac55b40a9c481904a9fc
Ravenshard <jechmila@ualberta.ca>
2018-03-07 16:17:42 -0700

    Commenting TaskPasser.java, should be 'done'

--- app/src/main/java/com/c301t3/c301t3app/TaskPasser.java
+++ app/src/main/java/com/c301t3/c301t3app/TaskPasser.java
@@ -10,18 +10,31 @@ import java.util.ArrayList;
 public class TaskPasser {
     private static ArrayList<Task> tasks;
 
+    /**
+     * Create a new ArrayList when creating a TaskPasser
+     */
     public TaskPasser() {
         tasks = new ArrayList<>();
     }
-
+    /**
+     * Get the ArrayList passed, ALL the items
+     *
+     * @return  tasks; the ArrayList of information to pass
+     */
     public ArrayList<Task> getTasks() {
         return tasks;
     }
 
+    /**
+     * Set tasks to the ArrayList passed, t
+     *
+     * @param t; Set the tasks to t, disregarding all previous tasks
+     */
     public void setTasks(ArrayList<Task> t) {
         tasks = t;
     }
 
+    /**
     public Task getTask() {
         return tasks.get(0);
     }
@@ -30,4 +43,5 @@ public class TaskPasser {
         tasks = new ArrayList<>();
         tasks.add(t);
     }
+    */
 }
