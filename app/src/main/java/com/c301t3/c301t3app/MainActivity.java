eb2d37b8ea8f1d71307566abd442e0f2eb05d554
xwafflecakex <nyevtukh@gmail.com>
2018-03-06 19:33:22 -0800

    Fixed XML files and some test java  code that was preventing the thing from building.

--- app/src/main/java/com/c301t3/c301t3app/MainActivity.java
+++ app/src/main/java/com/c301t3/c301t3app/MainActivity.java
@@ -1,18 +1,15 @@
 package com.c301t3.c301t3app;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    
+
     public static final int MYTASKCODE = 1;
     /* Here is a good site with a good tutorial for back button and info sharing between activities.
     * https://google-developer-training.gitbooks.io/android-developer-fundamentals-course-practicals/content/en/Unit%201/21_p_create_and_start_activities.html*/
@@ -23,13 +20,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button GoToMyTasks = (Button) findViewById(R.id.button_GoToMyTasks);
+
+        /* This is all garbage test code below
+
+         */
+        Button GoToMyTasks = findViewById(R.id.button_GoToMyTasks);
 
         GoToMyTasks.setOnClickListener( new View.OnClickListener() {
 
             public void onClick(View view) {
                 setResult(RESULT_OK);
-                Intent intent = new Intent(view.getContext(), MyTasksActivity.class);
+                Intent intent = new Intent(view.getContext(), Task.class);
 
                 Task task0 = new Task("task0", "description for task0", "okay");
                 Task task1 = new Task("task1", "description for task1", "meh");
@@ -39,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
                 tasklist.addTask(task1);
                 tasklist.addTask(task2);
 
-                 intent.putExtra("SentTaskList", tasklist);
+                intent.putExtra("SentTaskList", tasklist);
                 startActivityForResult(intent, MYTASKCODE);
             }
 
