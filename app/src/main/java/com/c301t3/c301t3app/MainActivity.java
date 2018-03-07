313d951d0d3eb8e604c0014a3c193739194dd7fe
Kiefer <kiefer@vongaza.ca>
2018-03-07 15:37:44 -0700

    Fix MainActivity junk

--- app/src/main/java/com/c301t3/c301t3app/MainActivity.java
+++ app/src/main/java/com/c301t3/c301t3app/MainActivity.java
@@ -9,49 +9,15 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    public static final int MYTASKCODE = 1;
     /* Here is a good site with a good tutorial for back button and info sharing between activities.
     * https://google-developer-training.gitbooks.io/android-developer-fundamentals-course-practicals/content/en/Unit%201/21_p_create_and_start_activities.html*/
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        /* This is all garbage test code below
-
-         */
-        Button GoToMyTasks = findViewById(R.id.button_GoToMyTasks);
-
-        GoToMyTasks.setOnClickListener( new View.OnClickListener() {
-
-            public void onClick(View view) {
-                setResult(RESULT_OK);
-                Intent intent = new Intent(view.getContext(), Task.class);
-
-                Task task0 = new Task("task0", "description for task0", "okay");
-                Task task1 = new Task("task1", "description for task1", "meh");
-                Task task2 = new Task("task2", "description for task2", "cool");
-                TaskList tasklist = new TaskList();
-                tasklist.addTask(task0);
-                tasklist.addTask(task1);
-                tasklist.addTask(task2);
-
-                intent.putExtra("SentTaskList", tasklist);
-                startActivityForResult(intent, MYTASKCODE);
-            }
-
-        });
-
     }
 
-    public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG, "Button clicked!");
-        Intent intent = new Intent(this, selected_task_activity.class);
-        startActivity(intent);
-    }
 }
 
