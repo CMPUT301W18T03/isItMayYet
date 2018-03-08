0183887a180263b5412367b94371b8b5fb4ba079
Henry Lam <nkb012pomm@gmail.com>
2018-03-07 20:51:17 -0700

    Done more on MyTasksActivity

--- app/src/main/java/com/c301t3/c301t3app/MyTasksActivity.java
+++ app/src/main/java/com/c301t3/c301t3app/MyTasksActivity.java
@@ -2,6 +2,10 @@ package com.c301t3.c301t3app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Henry on 2018-03-07.
@@ -9,10 +13,27 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MyTasksActivity extends AppCompatActivity {
 
+    private final TaskPasser taskPasser = new TaskPasser();
+    private ArrayList<Task> taskList;
+    private ArrayAdapter<Task> adapter;
+    private ListView myTasksList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.selected_task);
+        setContentView(R.layout.activity_my_tasks);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        myTasksList = (ListView) findViewById(R.id.myTasksList);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        taskList = taskPasser.getTasks();
+        adapter = new ArrayAdapter<Task>(this, R.layout.my_tasks_item, taskList);
+        myTasksList.setAdapter(adapter);
     }
 
 }
