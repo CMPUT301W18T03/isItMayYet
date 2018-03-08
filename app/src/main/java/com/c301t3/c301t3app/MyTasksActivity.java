5f2c379bebfbae575be833af533a5604e4cd7343
Henry Lam <nkb012pomm@gmail.com>
2018-03-07 20:37:20 -0700

    Creation of MyTasksActivity

--- app/src/main/java/com/c301t3/c301t3app/MyTasksActivity.java
+++ app/src/main/java/com/c301t3/c301t3app/MyTasksActivity.java
@@ -1,8 +1,18 @@
 package com.c301t3.c301t3app;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Henry on 2018-03-07.
  */
 
-public class MyTasksActivity {
+public class MyTasksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setContentView(R.layout.selected_task);
+    }
+
 }
