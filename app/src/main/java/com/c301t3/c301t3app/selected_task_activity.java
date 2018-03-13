57f189d4ada149d3543822291517f3c334ced064
xwafflecakex <nyevtukh@gmail.com>
2018-03-12 18:57:04 -0700

    Put the view identfications for views, catching up to henry's # of commits.

--- app/src/main/java/com/c301t3/c301t3app/selected_task_activity.java
+++ app/src/main/java/com/c301t3/c301t3app/selected_task_activity.java
@@ -6,13 +6,29 @@ package com.c301t3.c301t3app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class selected_task_activity extends AppCompatActivity {
+
+    private TaskPasser passer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.selected_task);
+        final TaskPasser passer = new TaskPasser();
+
+        TextView taskName = findViewById(R.id.textViewTaskName);
+        TextView taskDesc = findViewById(R.id.textViewTaskDes);
+        ImageView taskImages = findViewById(R.id.imageView);
+        TextView taskStat = findViewById(R.id.textViewStatus);
+        TextView taskPrice = findViewById(R.id.textViewPrice);
+        EditText taskBid = findViewById(R.id.editTextBid);
+        Button taskBidBtn = findViewById(R.id.bidBtn);
+
     }
 }
