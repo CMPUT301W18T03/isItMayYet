8c813de7be6177aad4e02489403299e74a7ed019
xwafflecakex <nyevtukh@gmail.com>
2018-03-12 19:13:47 -0700

    Put the set texts, and a conditional on bid btn

--- app/src/main/java/com/c301t3/c301t3app/selected_task_activity.java
+++ app/src/main/java/com/c301t3/c301t3app/selected_task_activity.java
@@ -6,6 +6,8 @@ package com.c301t3.c301t3app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -15,12 +17,14 @@ import android.widget.TextView;
 public class selected_task_activity extends AppCompatActivity {
 
     private TaskPasser passer;
+    private Task currentTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.selected_task);
         final TaskPasser passer = new TaskPasser();
+        //get current selected task.
 
         TextView taskName = findViewById(R.id.textViewTaskName);
         TextView taskDesc = findViewById(R.id.textViewTaskDes);
@@ -29,6 +33,25 @@ public class selected_task_activity extends AppCompatActivity {
         TextView taskPrice = findViewById(R.id.textViewPrice);
         EditText taskBid = findViewById(R.id.editTextBid);
         Button taskBidBtn = findViewById(R.id.bidBtn);
+
+        taskBidBtn.setEnabled(false);
+        taskName.setText(currentTask.getTaskName());
+        taskDesc.setText(currentTask.getTaskDescription());
+        //taskImages.setImageResource(Drawablesres);
+        taskStat.setText(currentTask.getStatus());
+        taskPrice.setText(currentTask.getPrice());
+        try {
+            taskBid.setText(currentTask.getCurrentBid);
+        } catch (Exception e) {
+            Log.i("Bid error", "getting current bid error.");
+            e.printStackTrace();
+        }
+
+    }
+
+    public void taskBidded(View view) {
+        Log.i("button is pressed", "bid button is pressed.");
+        // If the status of the bid is Green, btn is active.
 
     }
 }
