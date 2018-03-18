772824767831481f2a99aa06b3de49cb0207eced
alido8592 <alido@ualberta.ca>
2018-03-18 15:52:46 -0600

    main menu and custom adapter

--- app/src/main/java/com/c301t3/c301t3app/MainMenuActivity.java
+++ app/src/main/java/com/c301t3/c301t3app/MainMenuActivity.java
@@ -1,9 +1,13 @@
 package com.c301t3.c301t3app;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -22,12 +26,15 @@ public class MainMenuActivity extends AppCompatActivity{
     private MainMenuActivity activity = this;
     private final TaskPasser taskPasser = new TaskPasser();
     public TaskList taskList = new TaskList(); //sample list TODO: remove, maybe
+    private final Context context = MainMenuActivity.this;
 
-    private ListView taskListView;
-    private ArrayAdapter adapter;
+    private RecyclerView taskListView;
+    private TasksRequestedAdapter adapter;
     private FloatingActionButton addTaskButton;
     private EditText searchInput;
     private ArrayList<Task> tasks = new ArrayList<Task>();
+
+    private int position;
 
 
     /// Menu Start Here-----------------------
@@ -92,13 +99,13 @@ public class MainMenuActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu_activity);
 
-        addTaskButton = findViewById(R.id.addTaskButton);
-        taskListView = findViewById(R.id.listView);
+        addTaskButton = (FloatingActionButton) findViewById(R.id.addTaskButton);
+        taskListView = (RecyclerView) findViewById(R.id.tasksView);
 
         //debug
-        Task task0 = new Task("Task0","Desc0",TaskStatus.REQUESTED);
-        Task task1 = new Task("Task1","Desc1",TaskStatus.REQUESTED);
-        Task task2 = new Task("Task2","Desc2",TaskStatus.ASSIGNED);
+        Task task0 = new Task("Task0","Description for task0",TaskStatus.REQUESTED,15);
+        Task task1 = new Task("Task1","There isn't really any reason to describe this",TaskStatus.REQUESTED,20);
+        Task task2 = new Task("Task2","Desc2",TaskStatus.ASSIGNED,20);
         taskList.addTask(task0);
         taskList.addTask(task1);
         taskList.addTask(task2);
@@ -119,14 +126,31 @@ public class MainMenuActivity extends AppCompatActivity{
     @Override
     protected void onStart() {
         super.onStart();
-        taskListView = findViewById(R.id.listView);
-        searchInput = findViewById(R.id.searchBar);
+        taskListView = (RecyclerView) findViewById(R.id.tasksView);
+        searchInput = (EditText) findViewById(R.id.searchBar);
         searchInput.setOnKeyListener(searchTasks);
 
-        adapter = new ArrayAdapter(this, R.layout.task_item,tasks);
+        adapter = new TasksRequestedAdapter(this,tasks);
+        RecyclerView.ItemDecoration itemDeco = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        taskListView.addItemDecoration(itemDeco);
+        RecyclerView.LayoutManager layoutMan = new LinearLayoutManager(this);
+        layoutMan.setAutoMeasureEnabled(true);
+
         taskListView.setAdapter(adapter);
+        taskListView.setLayoutManager(layoutMan);
 
         adapter.notifyDataSetChanged();
+
+        adapter.setOnItemClickListener(new TasksRequestedAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View singleTask, int pos) {
+                position = pos;
+                Intent selectedIntent = new Intent(context, SelectedTaskActivity.class);
+                Task selTask = adapter.getItem(pos);
+                selectedIntent.putExtra("MainSelectedTask",selTask);
+                startActivity(selectedIntent);
+            }
+        });
     }
 
     EditText.OnKeyListener searchTasks = new EditText.OnKeyListener() {
@@ -143,9 +167,12 @@ public class MainMenuActivity extends AppCompatActivity{
                     Toast.makeText(getApplicationContext(),"Error: Please enter a search query",Toast.LENGTH_SHORT).show();
                     return false;
                 }
+
                 else {
                     for (i=0;i<taskList.getTaskList().size();i++) { // works for hardcoded short list of tasks.. takes long for more content
-                        if ((taskList.getTask(i).getDescription().toLowerCase().contains(searchWord)) && ((taskList.getTask(i).getStatus()==TaskStatus.REQUESTED) || (taskList.getTask(i).getStatus()==TaskStatus.BIDDED))) {
+                        if ((taskList.getTask(i).getDescription().toLowerCase().contains(searchWord))
+                                && ((taskList.getTask(i).getStatus()==TaskStatus.REQUESTED)
+                                || (taskList.getTask(i).getStatus()==TaskStatus.BIDDED))) {
                             searchMatch.add(taskList.getTask(i));
                         }
                     }
