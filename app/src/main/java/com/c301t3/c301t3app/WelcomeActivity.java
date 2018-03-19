d8453e1eb6d78977f2a2ba211b37e3fbc8402e24
Henry Lam <nkb012pomm@gmail.com>
2018-03-19 15:41:35 -0600

    Added in some functionality for JsonHandler

--- app/src/main/java/com/c301t3/c301t3app/WelcomeActivity.java
+++ app/src/main/java/com/c301t3/c301t3app/WelcomeActivity.java
@@ -23,7 +23,7 @@ public class WelcomeActivity extends AppCompatActivity {
     private Intent mainMenuIntent;
     /* Here is a good site with a good tutorial for back button and info sharing between activities.
     * https://google-developer-training.gitbooks.io/android-developer-fundamentals-course-practicals/content/en/Unit%201/21_p_create_and_start_activities.html*/
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
