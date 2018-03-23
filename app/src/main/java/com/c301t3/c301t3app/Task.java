ef88329899ed60c23b7e969e38ea463f363c521d
Jonah Quist <jonahquist@gmail.com>
2018-03-23 12:38:13 -0600

    for real this time

--- app/src/main/java/com/c301t3/c301t3app/Task.java
+++ app/src/main/java/com/c301t3/c301t3app/Task.java
@@ -20,6 +20,7 @@ public class Task implements Serializable {
     private TaskStatus status;
     private int price;
     private ArrayList<Bid> bids;
+    private String id;
 
     // Photo photo;     // Commented out from not knowing how/what to represent photo with.
     // GeoLoc location;     // Commented out from not knowing how/what to represent location with.
