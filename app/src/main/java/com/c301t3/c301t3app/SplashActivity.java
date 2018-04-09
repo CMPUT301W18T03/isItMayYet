439d75af4589712a23d0f47ef6f5194811a7ee01
Ravenshard <jechmila@ualberta.ca>
2018-04-09 15:33:56 -0600

    Still adding stuff comments and refactoring

--- app/src/main/java/com/c301t3/c301t3app/SplashActivity.java
+++ app/src/main/java/com/c301t3/c301t3app/SplashActivity.java
@@ -4,8 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/** Splash Activity, the load screen for when the app is loading
+ *
+ */
 public class SplashActivity extends AppCompatActivity {
 
+    /** Called when the view is first created,
+     *  Simply displays the app logo and waits to load
+     *
+     * @param savedInstanceState; previous data, nothing probably
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
