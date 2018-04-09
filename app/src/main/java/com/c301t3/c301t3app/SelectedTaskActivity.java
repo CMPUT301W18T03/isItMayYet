cd25a9b65eebb97bc95465efd89b851c51a9dd8d
Henry Lam <nkb012pomm@gmail.com>
2018-04-08 21:59:31 -0600

    I got it so that MyBids won't crash when the user interacts with it.

--- app/src/main/java/com/c301t3/c301t3app/SelectedTaskActivity.java
+++ app/src/main/java/com/c301t3/c301t3app/SelectedTaskActivity.java
@@ -15,6 +15,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.StringTokenizer;
